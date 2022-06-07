dd68f26053a2d9b5d53fbbade128d0c76df12602
patipat <lpatipat@icloud.com>
2022-06-07 16:01:17 -0400

    update drop end points to new sage domain

--- packages/upload-app/src/services/config.ts
+++ packages/upload-app/src/services/config.ts
@@ -8,16 +8,16 @@ export interface Configuration {
 
 export const targetConfig: Configuration = {
   // IMPORTANT: URLs must end with a forward slash ("/") - or NextJS will redirect, causing CORS errors
-  "localhost": {
-    ENDPOINT_URL: "http://localhost:3001/api/dropUploadEndpoint/",
+  localhost: {
+    ENDPOINT_URL: 'http://localhost:3001/api/dropUploadEndpoint/',
   },
-  "URN Dev": {
-    ENDPOINT_URL: "https://urn-dev.vercel.app/api/dropUploadEndpoint/",
+  'URN Dev': {
+    ENDPOINT_URL: 'https://sage-dev.vercel.app/api/dropUploadEndpoint/',
   },
-  "URN Staging": {
-    ENDPOINT_URL: "https://urn-dev.vercel.app/api/dropUploadEndpoint/",
+  'URN Staging': {
+    ENDPOINT_URL: 'https://sage-dev.vercel.app/api/dropUploadEndpoint/',
   },
-  "URN Production": {
-    ENDPOINT_URL: "https://urn-dev.vercel.app/api/dropUploadEndpoint/",
+  'URN Production': {
+    ENDPOINT_URL: 'https://sage-dev.vercel.app/api/dropUploadEndpoint/',
   },
 };
